978faa8718afd0539df6a1eb3ae70073a92a2a68
Daniil1380 <Tkachenko1380@yandex.ru>
2020-10-12 17:54:43 +0300

    Добавлены интерфейсы

--- src/main/java/com.daniil1380.UnArch/MainProgram.java
+++ src/main/java/com.daniil1380.UnArch/MainProgram.java
@@ -1,5 +1,4 @@
 package main.java.com.daniil1380.UnArch;
-import java.io.IOException;
 
 public class MainProgram {
     public static void main(String[] args){
--- src/main/java/com.daniil1380.UnArch/ReaderDir.java
+++ src/main/java/com.daniil1380.UnArch/ReaderDir.java
@@ -1,7 +1,5 @@
 package main.java.com.daniil1380.UnArch;
 
-import java.io.IOException;
-
 public interface ReaderDir {
 
     void start();
